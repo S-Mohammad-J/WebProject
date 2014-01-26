b40a13ef34d4ef70d33375815f5c1c167019cb86
S-Mohammad-J <s.mohammad_ce88@yahoo.com>
2014-01-27 01:54:58 +0330

    template home.html

--- MyShop/views.py
+++ MyShop/views.py
@@ -3,12 +3,17 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from MyShop.models import SlideshowProduct
+from MyShop.models import SlideshowProduct, Section, SubSection
 
 
 def index(request):
     #return HttpResponse("Hello World, I'm here")
     slideshowProducts = SlideshowProduct.objects.all()
-    context={"slideshowProducts":slideshowProducts}
+    sections=Section.objects.all()
+    subSections=SubSection.objects.all()
+    context={"slideshowProducts":slideshowProducts,
+             "sections":sections,
+             "subSections":subSections
+    }
     return render(request,"Home.html",context)
 
