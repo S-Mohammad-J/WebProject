5ac86a33298451525643bf85361b332b08ae02eb
S-Mohammad-J <s.mohammad_ce88@yahoo.com>
2014-01-26 18:11:02 +0330

    Model + DB is completed

--- MyShop/admin.py
+++ MyShop/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
 
 # Register your models here.
+from MyShop.models import Section, SubSection, Merchant, Customer, Product, SlideshowProduct, Comment, Bill
+
+class SectionAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
+
+class SubSectionAdmin(admin.ModelAdmin):
+    list_display = ("title","section")
+    list_filter = ["title"]
+    search_fields = ["title","section__title"]
+
+class MerchantAdmin(admin.ModelAdmin):
+    list_display = ("username","firstName","lastName","tel","email")
+    search_fields = ["useranme","firstName","lastName","tel","email","address"]
+
+class CustomertAdmin(admin.ModelAdmin):
+    list_display = ("username","firstName","lastName","tel","email")
+    search_fields = ["useranme","firstName","lastName","tel","email","address"]
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name","price","score","createDate","subSection","merchant")
+    search_fields = ["name","price","score","createDate","subSection__title","merchant__lastName","subSection__section__title"]
+
+class SlideshowProductAdmin(admin.ModelAdmin):
+    list_display = ("product","text")
+    search_fields = ["product__name","product__price","product__score","product__createDate","product__subSection__title","text"]
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("content","customer","product")
+    search_fields = ["content","customer__username","product__name"]
+
+class BillAdmin(admin.ModelAdmin):
+    list_display = ("customer","product")
+    search_fields = ["customer__username","product__name"]
+
+admin.site.register(Section,SectionAdmin)
+admin.site.register(SubSection,SubSectionAdmin)
+admin.site.register(Merchant,MerchantAdmin)
+admin.site.register(Customer,CustomertAdmin)
+admin.site.register(Product,ProductAdmin)
+admin.site.register(SlideshowProduct,SlideshowProductAdmin)
+admin.site.register(Comment,CommentAdmin)
+#TODO bill admin
+admin.site.register(Bill)
+
--- MyShop/models.py
+++ MyShop/models.py
@@ -1,3 +1,82 @@
+from django import forms
+from django.contrib.admin import widgets
 from django.db import models
 
+
 # Create your models here.
+
+class Section(models.Model):
+    title=models.CharField(max_length=150)
+    def __str__(self):
+        return self.title
+
+
+class SubSection(models.Model):
+    title=models.CharField(max_length=150)
+    section=models.ForeignKey(Section)
+    def __str__(self):
+        return self.title
+
+class User(models.Model):
+    username=models.CharField(max_length=150)
+    password=models.CharField(max_length=500)
+    firstName=models.CharField(max_length=150)
+    lastName=models.CharField(max_length=150)
+    address=models.TextField()
+    tel=models.CharField(max_length=20)
+    email=models.CharField(max_length=150)
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.username
+
+class Merchant(User):
+    sell_amount=models.PositiveIntegerField()
+
+
+
+class Customer(User):
+    favorite=models.TextField()
+
+
+class Product(models.Model):
+    name=models.CharField(max_length=150)
+    price=models.PositiveIntegerField()
+    picture=models.ImageField(upload_to="product_pic/")
+    score=models.PositiveIntegerField()
+    details=models.TextField()
+    createDate=models.DateTimeField()
+    subSection=models.ForeignKey(SubSection)
+    merchant=models.ForeignKey(Merchant)
+    def __str__(self):
+        return self.name
+
+
+
+class SlideshowProduct(models.Model):
+    text=models.CharField(max_length=500)
+    SlideshowPic=models.ImageField(upload_to="slideshow/")
+    product=models.ForeignKey(Product)
+    def __str__(self):
+        return self.product.name
+
+
+class Comment(models.Model):
+    content=models.TextField()
+    customer=models.ForeignKey(Customer)
+    product=models.ForeignKey(Product)
+    def __str__(self):
+        return self.content
+
+class Bill(models.Model):
+    customer=models.ForeignKey(Customer)
+    paidFlag=models.BooleanField()
+    price=models.PositiveIntegerField()
+    product=models.ManyToManyField(Product)
+    def __str__(self):
+        return self.customer.username + str(self.id)
+
+
+
+
